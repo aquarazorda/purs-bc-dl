c70bd3518e3f925383a6e6cb7d0f11f8c2d635a8
Givi Gelashvili <givi.gelashvili@adjarabet.com>
2021-09-02 11:57:40 +0400

    Fix for unavailable downloads crashing

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,11 @@
 exports.getRecord = (str) => {
     data = JSON.parse(str);
-    ret = data.track.itemListElement.map(item => ({
-        name: item.item.name,
-        link: item.item.additionalProperty[2].value
-    }));
-    
+    ret = data.track.itemListElement
+        .filter(item => typeof item.item.additionalProperty[2].value == "string")
+        .map(item => ({
+            name: item.item.name,
+            link: item.item.additionalProperty[2].value
+        }));
+
     return ret;
 }
